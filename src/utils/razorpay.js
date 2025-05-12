655182ffe8f1dc46e1d5f047739b8b767e8e4ffc
priyanshupandey12 <129362864+priyanshupandey12@users.noreply.github.com>
2025-05-12 21:57:03 +0530

    Update razorpay.js

--- src/utils/razorpay.js
+++ src/utils/razorpay.js
@@ -1,8 +1,8 @@
 const Razorpay=require('razorpay');
 
 var instance = new Razorpay({
-  key_id: 'rzp_test_849SIx3i6d8WzZ',
-  key_secret: 'vdoMxEnwYM4cYuzzhiAlQxyZ',
+  key_id: 'process.env.RAZORPAY_KEY_ID',
+  key_secret: 'process.env.RAZORPAY_KEY_SECRET',
 });
 
 
