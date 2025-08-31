57aad2ea92a1c1a256487b0ee5675540b1739398
priyanshupandey12 <priyanshupandat32@gmail.com>
2025-09-01 00:02:09 +0530

    update chat

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -1,34 +1,33 @@
 const Chat=require('../models/chat.model');
 
 
-const newchat=async(req,res)=>{
-  const {userId}=req.params;
 
-  const loggedin=req.user._id
-
+const newchat = async (req, res) => {
+  const { userId } = req.params;
+  const loggedin = req.user._id;
 
   try {
-    let chat=await Chat.findOne({
-      participants:{$all:[loggedin,userId]}
-    }).populate({
-      path:'messages.senderId',
-      select:'firstName'
+    let chat = await Chat.findOne({
+      participants: { $all: [loggedin, userId] }
     })
-    if(!chat) {
-      chat=await Chat.create({
-         participants:[loggedin,userId],
-         messages:[]
 
-       })
-       await chat.save();
-      }
-      res.status(200).json({message:"your messages",data:chat})
+    .populate('participants', 'firstName lastName photoUrl isOnline');
+
+    if (!chat) {
+     
+      const newChatData = {
+        participants: [loggedin, userId],
+        messages: []
+      };
+      chat = await (await Chat.create(newChatData)).populate('participants', 'firstName lastName photoUrl isOnline');
+    }
+    
+    res.status(200).json({ message: "your messages", data: chat });
     
   } catch (error) {
-    console.log(error)
-    return res.status(400).json("error in chat creating")
-    
+    console.log(error);
+    return res.status(400).json("error in chat creating");
   }
-}
+};
 
 module.exports={newchat}
