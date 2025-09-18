8d11c2cef7132acf2d19615db8887782688686b4
priyanshupandey12 <priyanshupandat32@gmail.com>
2025-09-18 18:10:29 +0530

    update env file

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const session = require('express-session');
 const passport = require('./src/utils/passport-config');
 app.use(cors
   ({
-    origin:'http://localhost:5173',
+    origin:process.env.FRONTEND_URL,
     credentials:true
   }
 ));
